9500f78850229bd50cd87bacc281644cd9cf84d9
krzm <kmazanek@gmail.com>
2022-04-13 12:30:56 +0200

    Add template app

---
+++ DiyBox.Modern.CliApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using DIHelper;
+using Modern.CLI.App.Template;
+using Unity;
+
+IBootstraper booter = new Bootstraper(
+    new UnityDependencySuite(
+        new UnityContainer().AddExtension(new Diagnostic())));
+booter.Boot(args);
